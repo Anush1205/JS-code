e99163a9a759221bdf76075bd9b6ffefff4730e8
Anush1205 <98580705+Anush1205@users.noreply.github.com>
2025-06-12 07:23:39 +0000

    variable

---
+++ .devcontainer/01_basics/01_Variable.js
@@ -0,0 +1,15 @@
+const accId = 475384
+let accName = "someone"
+var Branch = "Bangalore"
+state = "karnatak"
+
+//accId=45678
+accName="newEmploye"
+Branch="Gwalior"
+state="Madhya Pradesh"
+
+/*
+ prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accId,accName,Branch,state])
