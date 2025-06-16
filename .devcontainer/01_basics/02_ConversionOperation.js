5b24d5cd8460a2d737719a6ad2c95102b73b219b
Anush1205 <98580705+Anush1205@users.noreply.github.com>
2025-06-16 06:57:35 +0000

    conversion

---
+++ .devcontainer/01_basics/02_ConversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "33avc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value = Number(score);
+
+// console.log(typeof value)
+// console.log(value)
+
+//"33" => 33
+//"dja34" => NaN
+//trur => 1 ; false => 0
+
+let val = 1 
+let num = Boolean(val);
+
+console.log(typeof num);
+console.log(num);
+
+// 1 => true ; 2 => false
+// "" => false
+// "abc" => true
